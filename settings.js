fe6d40722753d325f0cebcfa3bdf28c43405bc7a
Ki <ikyyy659@gmail.com>
2025-04-29 11:41:25 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -17,7 +17,7 @@ global.listprefix = ['+','!','.']
 global.listv = ['•','●','■','✿','▲','➩','➢','➣','➤','✦','✧','△','❀','○','□','♤','♡','◇','♧','々','〆']
 global.tempatDB = 'database.json' // Taruh url mongodb di sini jika menggunakan mongodb. Format : 'mongodb+srv://...'
 global.pairing_code = true
-global.number_bot = '6283866945962' // Kalo pake panel bisa masukin nomer di sini, jika belum ambil session. Format : '628xx'
+global.number_bot = '' // masukan nomor bot mu
 
 global.fake = {
 	anonim: 'https://telegra.ph/file/95670d63378f7f4210f03.png',
